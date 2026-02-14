5e487400413a0e9ad96b922b07d4654bbf7f0eb5
proFQuansistor <profquansistor@seznam.cz>
2026-02-14 13:19:31 +0100

    Update juno.config.ts

--- juno.config.ts
+++ juno.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@junobuild/config";
 export default defineConfig({
   satellite: {
     ids: {
-      production: "tnsgl-4qaaa-aaaal-asw7q-cai"  // Tvůj satellite ID z Juno Console
+      production: "quchr-wyaaa-aaaal-aswwa-cai"  // Tvůj satellite ID z Juno Console
     },
     source: "build"  // Nahraje soubory z build/ složky
   }
